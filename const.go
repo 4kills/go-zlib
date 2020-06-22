60d0f3c547aa7d2d6a1728f64aa0a3dfcbddb174
4kills <dominik.ochs@gmx.net>
2020-06-22 14:07:12 +0200

    added the golang/zlib inspired abstraction and interface for the writer (compressor)

---
+++ const.go
@@ -0,0 +1,11 @@
+package zlib
+
+import "compress/flate"
+
+const (
+	NoCompression      = flate.NoCompression
+	BestSpeed          = flate.BestSpeed
+	BestCompression    = flate.BestCompression
+	DefaultCompression = flate.DefaultCompression
+	HuffmanOnly        = flate.HuffmanOnly
+)
