cb7f1bc1bfc8af1407cf49d9dda152b82f50072c
4kills <dominik.ochs@gmx.net>
2020-06-26 12:13:06 +0200

    fixed a bug in the reader so it buffers unread data and also returns number of processed bytes for readbytes

--- reader.go
+++ reader.go
@@ -2,6 +2,7 @@ package zlib
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 
 	"github.com/4kills/zlib/native"
@@ -11,6 +12,7 @@ import (
 type Reader struct {
 	r            io.Reader
 	decompressor *native.Decompressor
+	buffer       *bytes.Buffer
 }
 
 // Close closes the Reader by closing and freeing the underlying zlib stream.
@@ -19,25 +21,28 @@ func (r *Reader) Close() error {
 	if err := checkClosed(r.decompressor); err != nil {
 		return err
 	}
-
+	r.buffer = nil
 	return r.decompressor.Close()
 }
 
 // ReadBytes takes compressed data p, decompresses it and returns it as new byte slice.
+// It also returns the number n of bytes that were processed from the compressed slice.
+// If n < len(compressed) and err == nil then only the first n compressed bytes were in
+// a suitable zlib format.
 // This method is generally slightly faster than Read.
-func (r *Reader) ReadBytes(compressed []byte) ([]byte, error) {
+func (r *Reader) ReadBytes(compressed []byte) (n int, decompressed []byte, err error) {
 	if len(compressed) == 0 {
-		return nil, errNoInput
+		return 0, nil, errNoInput
 	}
 	if err := checkClosed(r.decompressor); err != nil {
-		return nil, err
+		return 0, nil, err
 	}
 
 	return r.decompressor.Decompress(compressed)
 }
 
 // Read reads compressed data from the provided Reader into the provided buffer p.
-// Please consider using ReadBytes instead, as it is faster
+// Please consider using ReadBytes instead, as it is faster and generally easier to use
 func (r *Reader) Read(p []byte) (int, error) {
 	if len(p) == 0 {
 		return 0, errNoInput
@@ -46,20 +51,29 @@ func (r *Reader) Read(p []byte) (int, error) {
 		return 0, err
 	}
 
-	bufSlice := make([]byte, 0, len(p))
-	buf := bytes.NewBuffer(bufSlice)
-	if _, err := io.Copy(buf, r.r); err != nil {
+	r.buffer.Grow(len(p))
+	if _, err := io.Copy(r.buffer, r.r); err != nil {
 		return 0, err
 	}
 
-	out, err := r.decompressor.Decompress(buf.Bytes())
+	fmt.Println(r.buffer.Len())
+
+	in := make([]byte, r.buffer.Len())
+	copy(in, r.buffer.Bytes())
+	processed, out, err := r.decompressor.Decompress(in)
 	if err != nil {
 		return 0, err
 	}
+	r.buffer.Next(processed)
+
+	fmt.Println(r.buffer.Len())
 
 	if len(out) <= len(p) {
 		copy(p, out)
-		return len(out), io.EOF
+		if r.buffer.Len() == 0 {
+			return len(out), io.EOF
+		}
+		return len(out), nil
 	}
 
 	copy(p, out[:len(p)])
@@ -74,6 +88,7 @@ func (r *Reader) Reset(reader io.Reader) {
 		panic(err)
 	}
 
+	r.buffer = &bytes.Buffer{}
 	r.r = reader
 }
 
@@ -81,5 +96,5 @@ func (r *Reader) Reset(reader io.Reader) {
 // r may be nil if you only plan on using ReadBytes
 func NewReader(r io.Reader) (*Reader, error) {
 	c, err := native.NewDecompressor()
-	return &Reader{r, c}, err
+	return &Reader{r, c, &bytes.Buffer{}}, err
 }
