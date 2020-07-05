c3b622d0a7708b7a60d5adf799b3b735753ed91a
4kills <dominik.ochs@gmx.net>
2020-07-05 22:36:46 +0200

    added platform independant toInt functions

---
+++ native/int_unix.go
@@ -0,0 +1,14 @@
+// +build !windows
+
+package native
+
+// #include <stdint.h>
+import "C"
+
+func toInt64(in int64) C.int64_t {
+	return C.long(in)
+}
+
+func intToInt64(in int) C.int64_t {
+	return toInt64(int64(in))
+}
---
+++ native/int_windows.go
@@ -0,0 +1,14 @@
+// +build windows
+
+package native
+
+// #include <stdint.h>
+import "C"
+
+func toInt64(in int64) C.int64_t {
+	return C.longlong(in)
+}
+
+func intToInt64(in int) C.int64_t {
+	return toInt64(int64(in))
+}
