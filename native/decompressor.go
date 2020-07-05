d33d1b3f36e6c9645e05b7d023920108de3ec73b
4kills <dominik.ochs@gmx.net>
2020-07-05 23:21:23 +0200

    added linker flags for macos

--- native/decompressor.go
+++ native/decompressor.go
@@ -4,6 +4,7 @@ package native
 #cgo CFLAGS: -I${SRCDIR}/zlib/
 #cgo windows LDFLAGS: ${SRCDIR}/libs/winlibz.a
 #cgo linux LDFLAGS: ${SRCDIR}/libs/linuxlibz.a
+#cgo darwin LDFLAGS: ${SRCDIR}/libs/darwinlibz.a
 
 #include "zlib.h"
 
