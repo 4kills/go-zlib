a028d49363df404bb7d3b76ce3fd6f7e25b53d2b
4kills <dominik.ochs@gmx.net>
2020-07-06 00:17:04 +0200

    possibly found another way of making int conversion platform independant

---
+++ native/int.go
@@ -0,0 +1,20 @@
+package native
+
+/* #include <stdint.h>
+int getSizeOfLong() {
+	return sizeof(long);
+}
+
+int64_t convert(long long integer) {
+	return (int64_t) integer;
+}
+*/
+import "C"
+
+func toInt64(in int64) C.int64_t {
+	return C.convert(C.longlong(in))
+}
+
+func intToInt64(in int) C.int64_t {
+	return toInt64(int64(in))
+}
