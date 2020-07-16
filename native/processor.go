2a956dc6e60e049c142b1f17ed650badcb4d8417
4kills <dominik.ochs@gmx.net>
2020-07-16 15:21:45 +0200

    moved c code to seperate files

--- native/processor.go
+++ native/processor.go
@@ -1,35 +1,7 @@
 package native
 
 /*
-#include "zlib.h"
-#include <stdlib.h>
-#include <stdint.h>
-
-typedef unsigned char b;
-
-z_stream* newStream() {
-	return (z_stream*) calloc(1, sizeof(z_stream));
-}
-
-void freeMem(z_stream* s) {
-	free(s);
-}
-
-int64_t getProcessed(z_stream* s, int64_t inSize) {
-	return inSize - s->avail_in;
-}
-
-int64_t getCompressed(z_stream* s, int64_t outSize) {
-	return outSize - s->avail_out;
-}
-
-void prepare(z_stream* s,  int64_t inPtr, int64_t inSize, int64_t outPtr, int64_t outSize) {
-    s->avail_in = inSize;
-    s->next_in = (b*) inPtr;
-
-    s->avail_out = outSize;
-    s->next_out = (b*) outPtr;
-}
+#include "processor.h"
 */
 import "C"
 import (
---
+++ native/processor.h
@@ -0,0 +1,15 @@
+#include "zlib.h"
+#include <stdlib.h>
+#include <stdint.h>
+
+typedef unsigned char b;
+
+z_stream* newStream();
+
+void freeMem(z_stream* s);
+
+int64_t getProcessed(z_stream* s, int64_t inSize);
+
+int64_t getCompressed(z_stream* s, int64_t outSize);
+
+void prepare(z_stream* s,  int64_t inPtr, int64_t inSize, int64_t outPtr, int64_t outSize); 
