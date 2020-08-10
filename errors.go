50b5f8e9bea6424575828efcb4230aeecb0da9b6
4kills <dominik.ochs@gmx.net>
2020-08-10 15:46:49 +0200

    changed errors to errors.New instead of fmt.Errorf, bcs the latter was not used to its capabilites

--- errors.go
+++ errors.go
@@ -1,12 +1,12 @@
 package zlib
 
 import (
-	"fmt"
+	"errors"
 )
 
 var (
-	errIsClosed        = fmt.Errorf("zlib: stream is already closed: you may not use this anymore")
-	errNoInput         = fmt.Errorf("zlib: no input provided: please provide at least 1 element")
-	errInvalidLevel    = fmt.Errorf("zlib: invalid compression level provided")
-	errInvalidStrategy = fmt.Errorf("zlib: invalid compression strategy provided")
+	errIsClosed        = errors.New("zlib: stream is already closed: you may not use this anymore")
+	errNoInput         = errors.New("zlib: no input provided: please provide at least 1 element")
+	errInvalidLevel    = errors.New("zlib: invalid compression level provided")
+	errInvalidStrategy = errors.New("zlib: invalid compression strategy provided")
 )
